2e125d925b9edfa652d27b42b2b85cd54284f700
Alain Meurant <alain.meurant@affinitic.be>
2015-10-14 23:47:27 +0300

    une skin onepage responsive

---
+++ src/forestcape/skin/browser/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('#accueil').parallax("center",0, true);
+    $('#about-us').parallax("center",0.5, true);
+    $('#projects').parallax("center",0.5, true);
+    $('#contact').parallax("center",0.5, true);
+})
+
